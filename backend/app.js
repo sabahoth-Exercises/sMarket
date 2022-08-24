c69cea5aa66cb0a6fb8b20d7786700936e653b1e
Brunnotte Wasaulua <bt.wasaulua@gmail.com>
2022-08-24 20:41:09 +0300

    implemented authentification

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
-const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 const app = express();
 app.use(express.json());
 mongoose.connect('mongodb+srv://tazi:brunnotte@cluster0.fhnseva.mongodb.net/?retryWrites=true&w=majority',
@@ -17,20 +19,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.use('/api/stuff', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }));
-});
+app.use(bodyParser.json())
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
